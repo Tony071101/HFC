7aa3aba7cbee804697b79e7158a80daff2b1b23e
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-04-22 16:52:23 +0700

    Create FormMenu.cs

---
+++ PresentationTier/FormMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace QLHFC.PresentationTier
+{
+    public partial class FormMenu : Form
+    {
+        
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void nHÂNVIÊNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormNV f = new FormNV();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void kHÁCHHÀNGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormKH f = new FormKH();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
+    }
+}
