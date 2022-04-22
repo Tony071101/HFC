ec85769b052f6c093d15db95e7b3cbde340bc163
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-04-22 16:51:57 +0700

    Create FormAdmin.cs

---
+++ PresentationTier/FormAdmin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHFC
+{
+    public partial class FormAdmin : Form
+    {
+        public FormAdmin()
+        {
+            InitializeComponent();
+            LoadAccountList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+             
+        }
+        void LoadAccountList()
+        {
+            
+        }
+    }
+}
