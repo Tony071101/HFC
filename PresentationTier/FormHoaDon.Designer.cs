32692181f8217aee8eaa54cd0c0f340003efea1a
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-05-18 14:47:46 +0700

    Create FormHoaDon.Designer.cs

---
+++ PresentationTier/FormHoaDon.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace QLHFC.PresentationTier
+{
+    partial class FormHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHoaDon));
+            this.dgvHD = new System.Windows.Forms.DataGridView();
+            this.btnCreateHoaDon = new System.Windows.Forms.Button();
+            this.txtSearchHD = new System.Windows.Forms.TextBox();
+            this.cmbSearchHD = new System.Windows.Forms.ComboBox();
+            this.btnDelHD = new System.Windows.Forms.Button();
+            this.btnUpdateHD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID_HD = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.cmbKH = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbNV = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHD)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvHD
+            // 
+            this.dgvHD.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHD.Location = new System.Drawing.Point(3, 177);
+            this.dgvHD.Name = "dgvHD";
+            this.dgvHD.RowHeadersWidth = 51;
+            this.dgvHD.RowTemplate.Height = 29;
+            this.dgvHD.Size = new System.Drawing.Size(1049, 489);
+            this.dgvHD.TabIndex = 2;
+            this.dgvHD.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHD_CellContentClick);
+            // 
+            // btnCreateHoaDon
+            // 
+            this.btnCreateHoaDon.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCreateHoaDon.Location = new System.Drawing.Point(659, 17);
+            this.btnCreateHoaDon.Name = "btnCreateHoaDon";
+            this.btnCreateHoaDon.Size = new System.Drawing.Size(127, 59);
+            this.btnCreateHoaDon.TabIndex = 3;
+            this.btnCreateHoaDon.Text = "Tạo";
+            this.btnCreateHoaDon.UseVisualStyleBackColor = true;
+            this.btnCreateHoaDon.Click += new System.EventHandler(this.btnCreateHoaDon_Click);
+            // 
+            // txtSearchHD
+            // 
+            this.txtSearchHD.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSearchHD.Location = new System.Drawing.Point(86, 133);
+            this.txtSearchHD.Name = "txtSearchHD";
+            this.txtSearchHD.Size = new System.Drawing.Size(433, 34);
+            this.txtSearchHD.TabIndex = 7;
+            this.txtSearchHD.TextChanged += new System.EventHandler(this.txtSearchHD_TextChanged);
+            // 
+            // cmbSearchHD
+            // 
+            this.cmbSearchHD.FormattingEnabled = true;
+            this.cmbSearchHD.Items.AddRange(new object[] {
+            "ID_HD",
+            "TenKH",
+            "TenNV"});
+            this.cmbSearchHD.Location = new System.Drawing.Point(12, 133);
+            this.cmbSearchHD.Name = "cmbSearchHD";
+            this.cmbSearchHD.Size = new System.Drawing.Size(68, 28);
+            this.cmbSearchHD.TabIndex = 8;
+            // 
+            // btnDelHD
+            // 
+            this.btnDelHD.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelHD.Location = new System.Drawing.Point(792, 17);
+            this.btnDelHD.Name = "btnDelHD";
+            this.btnDelHD.Size = new System.Drawing.Size(127, 59);
+            this.btnDelHD.TabIndex = 10;
+            this.btnDelHD.Text = "Xóa";
+            this.btnDelHD.UseVisualStyleBackColor = true;
+            this.btnDelHD.Click += new System.EventHandler(this.btnDelHD_Click);
+            // 
+            // btnUpdateHD
+            // 
+            this.btnUpdateHD.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdateHD.Location = new System.Drawing.Point(925, 17);
+            this.btnUpdateHD.Name = "btnUpdateHD";
+            this.btnUpdateHD.Size = new System.Drawing.Size(127, 59);
+            this.btnUpdateHD.TabIndex = 11;
+            this.btnUpdateHD.Text = "Sửa";
+            this.btnUpdateHD.UseVisualStyleBackColor = true;
+            this.btnUpdateHD.Click += new System.EventHandler(this.btnUpdateHD_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 28);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "ID:";
+            // 
+            // txtID_HD
+            // 
+            this.txtID_HD.BackColor = System.Drawing.Color.Silver;
+            this.txtID_HD.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtID_HD.Location = new System.Drawing.Point(53, 9);
+            this.txtID_HD.Name = "txtID_HD";
+            this.txtID_HD.ReadOnly = true;
+            this.txtID_HD.Size = new System.Drawing.Size(70, 34);
+            this.txtID_HD.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 28);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "TÊN KHÁCH HÀNG:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 28);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "NGÀY:";
+            // 
+            // txtDate
+            // 
+            this.txtDate.BackColor = System.Drawing.Color.Silver;
+            this.txtDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtDate.Location = new System.Drawing.Point(86, 82);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.ReadOnly = true;
+            this.txtDate.Size = new System.Drawing.Size(433, 34);
+            this.txtDate.TabIndex = 18;
+            // 
+            // cmbKH
+            // 
+            this.cmbKH.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbKH.FormattingEnabled = true;
+            this.cmbKH.Location = new System.Drawing.Point(190, 43);
+            this.cmbKH.Name = "cmbKH";
+            this.cmbKH.Size = new System.Drawing.Size(329, 36);
+            this.cmbKH.TabIndex = 25;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPrint);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.cmbNV);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.cmbKH);
+            this.panel1.Controls.Add(this.txtDate);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtID_HD);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnUpdateHD);
+            this.panel1.Controls.Add(this.btnDelHD);
+            this.panel1.Controls.Add(this.cmbSearchHD);
+            this.panel1.Controls.Add(this.txtSearchHD);
+            this.panel1.Controls.Add(this.btnCreateHoaDon);
+            this.panel1.Controls.Add(this.dgvHD);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1055, 678);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(792, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 52);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "CHI TIẾT HÓA ĐƠN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbNV
+            // 
+            this.cmbNV.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbNV.FormattingEnabled = true;
+            this.cmbNV.Location = new System.Drawing.Point(773, 82);
+            this.cmbNV.Name = "cmbNV";
+            this.cmbNV.Size = new System.Drawing.Size(279, 36);
+            this.cmbNV.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(659, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 28);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Họ Tên NV:";
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPrint.Location = new System.Drawing.Point(659, 124);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(127, 52);
+            this.btnPrint.TabIndex = 29;
+            this.btnPrint.Text = "In";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // FormHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 678);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormHoaDon";
+            this.Load += new System.EventHandler(this.FormHoaDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHD)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dgvHD;
+        private Button btnCreateHoaDon;
+        private TextBox txtSearchHD;
+        private ComboBox cmbSearchHD;
+        private Button btnDelHD;
+        private Button btnUpdateHD;
+        private Label label1;
+        private TextBox txtID_HD;
+        private Label label2;
+        private Label label3;
+        private TextBox txtDate;
+        private ComboBox cmbKH;
+        private Panel panel1;
+        private Label label4;
+        private ComboBox cmbNV;
+        private Button button1;
+        private Button btnPrint;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private PrintPreviewDialog printPreviewDialog1;
+    }
+}
