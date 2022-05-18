55a9597ba71a4bbe7c9feb656167fed93d876c64
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-05-18 14:48:27 +0700

    Create FormThucDon.Designer.cs

---
+++ PresentationTier/FormThucDon.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace QLHFC.PresentationTier
+{
+    partial class FormThucDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtSearchMonAn = new System.Windows.Forms.TextBox();
+            this.cmbSearchMonAn = new System.Windows.Forms.ComboBox();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.txtTenMon = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIDMon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdateFood = new System.Windows.Forms.Button();
+            this.btnDelFood = new System.Windows.Forms.Button();
+            this.btnAddFood = new System.Windows.Forms.Button();
+            this.dgvThucDon = new System.Windows.Forms.DataGridView();
+            this.mySqlDataAdapter1 = new MySqlConnector.MySqlDataAdapter();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThucDon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtSearchMonAn);
+            this.panel1.Controls.Add(this.cmbSearchMonAn);
+            this.panel1.Controls.Add(this.txtGia);
+            this.panel1.Controls.Add(this.txtTenMon);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtIDMon);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnUpdateFood);
+            this.panel1.Controls.Add(this.btnDelFood);
+            this.panel1.Controls.Add(this.btnAddFood);
+            this.panel1.Controls.Add(this.dgvThucDon);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1055, 678);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtSearchMonAn
+            // 
+            this.txtSearchMonAn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearchMonAn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSearchMonAn.Location = new System.Drawing.Point(673, 12);
+            this.txtSearchMonAn.Name = "txtSearchMonAn";
+            this.txtSearchMonAn.Size = new System.Drawing.Size(370, 34);
+            this.txtSearchMonAn.TabIndex = 19;
+            this.txtSearchMonAn.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // cmbSearchMonAn
+            // 
+            this.cmbSearchMonAn.FormattingEnabled = true;
+            this.cmbSearchMonAn.Items.AddRange(new object[] {
+            "ID_MonAn",
+            "TenMonAn",
+            "Gia"});
+            this.cmbSearchMonAn.Location = new System.Drawing.Point(570, 12);
+            this.cmbSearchMonAn.Name = "cmbSearchMonAn";
+            this.cmbSearchMonAn.Size = new System.Drawing.Size(97, 28);
+            this.cmbSearchMonAn.TabIndex = 18;
+            // 
+            // txtGia
+            // 
+            this.txtGia.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtGia.Location = new System.Drawing.Point(108, 98);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(404, 34);
+            this.txtGia.TabIndex = 11;
+            // 
+            // txtTenMon
+            // 
+            this.txtTenMon.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtTenMon.Location = new System.Drawing.Point(108, 54);
+            this.txtTenMon.Name = "txtTenMon";
+            this.txtTenMon.Size = new System.Drawing.Size(404, 34);
+            this.txtTenMon.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 28);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Giá:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Tên món:";
+            // 
+            // txtIDMon
+            // 
+            this.txtIDMon.BackColor = System.Drawing.Color.Silver;
+            this.txtIDMon.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtIDMon.Location = new System.Drawing.Point(108, 9);
+            this.txtIDMon.Name = "txtIDMon";
+            this.txtIDMon.ReadOnly = true;
+            this.txtIDMon.Size = new System.Drawing.Size(404, 34);
+            this.txtIDMon.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID:";
+            // 
+            // btnUpdateFood
+            // 
+            this.btnUpdateFood.Location = new System.Drawing.Point(936, 81);
+            this.btnUpdateFood.Name = "btnUpdateFood";
+            this.btnUpdateFood.Size = new System.Drawing.Size(107, 48);
+            this.btnUpdateFood.TabIndex = 4;
+            this.btnUpdateFood.Text = "Sửa";
+            this.btnUpdateFood.UseVisualStyleBackColor = true;
+            this.btnUpdateFood.Click += new System.EventHandler(this.btnUpdateFood_Click);
+            // 
+            // btnDelFood
+            // 
+            this.btnDelFood.Location = new System.Drawing.Point(758, 81);
+            this.btnDelFood.Name = "btnDelFood";
+            this.btnDelFood.Size = new System.Drawing.Size(104, 48);
+            this.btnDelFood.TabIndex = 3;
+            this.btnDelFood.Text = "Xóa";
+            this.btnDelFood.UseVisualStyleBackColor = true;
+            this.btnDelFood.Click += new System.EventHandler(this.btnDelFood_Click);
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Location = new System.Drawing.Point(573, 81);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(114, 48);
+            this.btnAddFood.TabIndex = 2;
+            this.btnAddFood.Text = "Thêm";
+            this.btnAddFood.UseVisualStyleBackColor = true;
+            this.btnAddFood.Click += new System.EventHandler(this.btnAddFood_Click);
+            // 
+            // dgvThucDon
+            // 
+            this.dgvThucDon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvThucDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThucDon.Location = new System.Drawing.Point(12, 198);
+            this.dgvThucDon.Name = "dgvThucDon";
+            this.dgvThucDon.RowHeadersWidth = 51;
+            this.dgvThucDon.RowTemplate.Height = 29;
+            this.dgvThucDon.Size = new System.Drawing.Size(1031, 468);
+            this.dgvThucDon.TabIndex = 0;
+            this.dgvThucDon.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThucDon_CellContentClick);
+            // 
+            // mySqlDataAdapter1
+            // 
+            this.mySqlDataAdapter1.DeleteCommand = null;
+            this.mySqlDataAdapter1.InsertCommand = null;
+            this.mySqlDataAdapter1.SelectCommand = null;
+            this.mySqlDataAdapter1.UpdateBatchSize = 0;
+            this.mySqlDataAdapter1.UpdateCommand = null;
+            // 
+            // FormThucDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 678);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormThucDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormThucDon";
+            this.Load += new System.EventHandler(this.FormThucDon_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThucDon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private MySqlConnector.MySqlDataAdapter mySqlDataAdapter1;
+        private Button btnUpdateFood;
+        private Button btnDelFood;
+        private Button btnAddFood;
+        private DataGridView dgvThucDon;
+        private TextBox txtGia;
+        private TextBox txtTenMon;
+        private Label label3;
+        private Label label2;
+        private TextBox txtIDMon;
+        private Label label1;
+        private ComboBox cmbSearchMonAn;
+        private TextBox txtSearchMonAn;
+    }
+}
