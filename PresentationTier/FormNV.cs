66620b38cf9202d72fa81181cfe9647a5e494dfd
Tony071101 <tinpigboyapple07112001@gmail.com>
2022-05-19 23:22:46 +0700

    Update FormNV.cs

--- PresentationTier/FormNV.cs
+++ PresentationTier/FormNV.cs
@@ -41,12 +41,20 @@ namespace QLHFC.PresentationTier
 
                 MessageBox.Show("Lỗi kết nối MySQL.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            DataGridViewImageColumn pic = new DataGridViewImageColumn();
+            pic = (DataGridViewImageColumn)dgvNV.Columns[6];
+            pic.ImageLayout = DataGridViewImageCellLayout.Stretch;
         }
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] pt = ms.ToArray();
             conn.Open();
-            string query_insert = "INSERT INTO hfc.nhanvien (TenNV,DiaChiNV,SDT_NV,Username,Password) VALUES ('" + txtName_NV.Text + "','" + txtAddress_NV.Text + "','" + txtSDT_NV.Text + "','" + txtUsername_NV.Text + "','" + txtPassword_NV.Text + "')";
+            string query_insert = "INSERT INTO hfc.nhanvien (TenNV,DiaChiNV,SDT_NV,Username,Password,AnhNV) VALUES ('" + txtName_NV.Text + "','" + txtAddress_NV.Text + "','" + txtSDT_NV.Text + "','" + txtUsername_NV.Text + "','" + txtPassword_NV.Text + "', @AnhNV)";
             MySqlCommand command = new MySqlCommand(query_insert, conn);
+            command.Parameters.Add("@AnhNV", MySqlDbType.LongBlob);
+            command.Parameters["@AnhNV"].Value = pt;
             command.ExecuteNonQuery();
             conn.Close();
             Read_Data();
@@ -63,9 +71,14 @@ namespace QLHFC.PresentationTier
         }
         private void btnEdit_Click(object sender, EventArgs e)
         {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] pt = ms.ToArray();
             conn.Open();
-            string query_edit = "UPDATE hfc.nhanvien SET TenNV ='" + txtName_NV.Text + "', DiaChiNV ='" + txtAddress_NV.Text + "',SDT_NV ='" + txtSDT_NV.Text + "',Username ='" + txtUsername_NV.Text + "',Password ='" + txtPassword_NV.Text + "' WHERE ID_NV = '" + int.Parse(txtID_NV.Text) + "'";
+            string query_edit = "UPDATE hfc.nhanvien SET TenNV ='" + txtName_NV.Text + "', DiaChiNV ='" + txtAddress_NV.Text + "',SDT_NV ='" + txtSDT_NV.Text + "',Username ='" + txtUsername_NV.Text + "',Password ='" + txtPassword_NV.Text + "', AnhNV = @AnhNV WHERE ID_NV = '" + int.Parse(txtID_NV.Text) + "'";
             MySqlCommand command = new MySqlCommand(query_edit, conn);
+            command.Parameters.Add("@AnhNV", MySqlDbType.LongBlob);
+            command.Parameters["@AnhNV"].Value = pt;
             command.ExecuteNonQuery();
             conn.Close();
             Read_Data();
@@ -89,6 +102,15 @@ namespace QLHFC.PresentationTier
             txtSDT_NV.Text = Convert.ToString(row.Cells["SDT_NV"].Value);
             txtUsername_NV.Text = Convert.ToString(row.Cells["Username"].Value);
             txtPassword_NV.Text = Convert.ToString(row.Cells["Password"].Value);
+            if (Convert.ToString(row.Cells["AnhNV"].Value) != "")
+            {
+                MemoryStream ms = new MemoryStream((byte[])row.Cells["AnhNV"].Value);
+                pictureBox1.Image = Image.FromStream(ms);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
         }
         private void txtSearch_TextChanged(object sender, EventArgs e)
         {
@@ -101,6 +123,21 @@ namespace QLHFC.PresentationTier
             adap.Fill(mytable);
             conn.Close();
             dgvNV.DataSource = mytable;
+        }
+
+        private void btnPic_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog odf = new OpenFileDialog();
+            odf.Title = "Chọn ảnh";
+            odf.Filter = "Image Files(*.gif;*.jpg;*.jpeg;*.bmp;*.wmf;*.png)|*.gif;*.jpg;*.jpeg;*.bmp;*.wmf;*.png";
+            if (odf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = odf.FileName;
+            }
+        }
+        private void dataGridView2_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            e.Cancel = true;
         }
     }
 }
