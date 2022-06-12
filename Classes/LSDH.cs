bfd34275f5e92b6e2a867a519e6d580d86a2c037
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-12 20:21:11 +0700

    Create LSDH.cs

---
+++ Classes/LSDH.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHFC.Classes
+{
+    public class LSDH
+    {
+        private static string tenkh;
+        public static string Tenkh
+        {
+            get { return tenkh; }
+            set { tenkh = value; }
+        }
+    }
+}
