1813e2cb36930ecd1ccb41b3d776d497f0ef866c
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-12 20:24:28 +0700

    Update FormMenu.Designer.cs

--- PresentationTier/FormMenu.Designer.cs
+++ PresentationTier/FormMenu.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.hÓAĐƠNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hÓAĐƠNToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.tHỰCĐƠNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.kHÁCHHÀNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nHÂNVIÊNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,61 +45,76 @@
             // 
             // menuStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.Color.LightGray;
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
             this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.hÓAĐƠNToolStripMenuItem,
+            this.hÓAĐƠNToolStripMenuItem1,
             this.tHỰCĐƠNToolStripMenuItem,
             this.kHÁCHHÀNGToolStripMenuItem,
             this.nHÂNVIÊNToolStripMenuItem,
             this.đĂNGXUẤTToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(147, 562);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(175, 749);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // hÓAĐƠNToolStripMenuItem
             // 
+            this.hÓAĐƠNToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.hÓAĐƠNToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("hÓAĐƠNToolStripMenuItem.Image")));
             this.hÓAĐƠNToolStripMenuItem.Name = "hÓAĐƠNToolStripMenuItem";
-            this.hÓAĐƠNToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
-            this.hÓAĐƠNToolStripMenuItem.Text = "HÓA ĐƠN";
+            this.hÓAĐƠNToolStripMenuItem.Size = new System.Drawing.Size(162, 29);
+            this.hÓAĐƠNToolStripMenuItem.Text = "QL HÓA ĐƠN";
             this.hÓAĐƠNToolStripMenuItem.Click += new System.EventHandler(this.hÓAĐƠNToolStripMenuItem_Click);
+            // 
+            // hÓAĐƠNToolStripMenuItem1
+            // 
+            this.hÓAĐƠNToolStripMenuItem1.ForeColor = System.Drawing.Color.Red;
+            this.hÓAĐƠNToolStripMenuItem1.Image = global::QLHFC.Properties.Resources.payment_bill_19;
+            this.hÓAĐƠNToolStripMenuItem1.Name = "hÓAĐƠNToolStripMenuItem1";
+            this.hÓAĐƠNToolStripMenuItem1.Size = new System.Drawing.Size(162, 29);
+            this.hÓAĐƠNToolStripMenuItem1.Text = "HÓA ĐƠN";
+            this.hÓAĐƠNToolStripMenuItem1.Click += new System.EventHandler(this.hÓAĐƠNToolStripMenuItem1_Click);
             // 
             // tHỰCĐƠNToolStripMenuItem
             // 
+            this.tHỰCĐƠNToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.tHỰCĐƠNToolStripMenuItem.Image = global::QLHFC.Properties.Resources.download__6_;
             this.tHỰCĐƠNToolStripMenuItem.Name = "tHỰCĐƠNToolStripMenuItem";
-            this.tHỰCĐƠNToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            this.tHỰCĐƠNToolStripMenuItem.Size = new System.Drawing.Size(162, 29);
             this.tHỰCĐƠNToolStripMenuItem.Text = "THỰC ĐƠN";
             this.tHỰCĐƠNToolStripMenuItem.Click += new System.EventHandler(this.tHỰCĐƠNToolStripMenuItem_Click);
             // 
             // kHÁCHHÀNGToolStripMenuItem
             // 
+            this.kHÁCHHÀNGToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.kHÁCHHÀNGToolStripMenuItem.Image = global::QLHFC.Properties.Resources.download__2_;
             this.kHÁCHHÀNGToolStripMenuItem.Name = "kHÁCHHÀNGToolStripMenuItem";
-            this.kHÁCHHÀNGToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
+            this.kHÁCHHÀNGToolStripMenuItem.Size = new System.Drawing.Size(162, 29);
             this.kHÁCHHÀNGToolStripMenuItem.Text = "KHÁCH HÀNG";
             this.kHÁCHHÀNGToolStripMenuItem.Click += new System.EventHandler(this.kHÁCHHÀNGToolStripMenuItem_Click);
             // 
             // nHÂNVIÊNToolStripMenuItem
             // 
+            this.nHÂNVIÊNToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.nHÂNVIÊNToolStripMenuItem.Image = global::QLHFC.Properties.Resources.download__3_1;
             this.nHÂNVIÊNToolStripMenuItem.Name = "nHÂNVIÊNToolStripMenuItem";
-            this.nHÂNVIÊNToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
+            this.nHÂNVIÊNToolStripMenuItem.Size = new System.Drawing.Size(162, 29);
             this.nHÂNVIÊNToolStripMenuItem.Text = "NHÂN VIÊN";
             this.nHÂNVIÊNToolStripMenuItem.Click += new System.EventHandler(this.nHÂNVIÊNToolStripMenuItem_Click);
             // 
             // đĂNGXUẤTToolStripMenuItem
             // 
+            this.đĂNGXUẤTToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.đĂNGXUẤTToolStripMenuItem.Image = global::QLHFC.Properties.Resources.download__4_1;
             this.đĂNGXUẤTToolStripMenuItem.Name = "đĂNGXUẤTToolStripMenuItem";
-            this.đĂNGXUẤTToolStripMenuItem.Size = new System.Drawing.Size(136, 24);
+            this.đĂNGXUẤTToolStripMenuItem.Size = new System.Drawing.Size(162, 29);
             this.đĂNGXUẤTToolStripMenuItem.Text = "ĐĂNG XUẤT";
             this.đĂNGXUẤTToolStripMenuItem.Click += new System.EventHandler(this.đĂNGXUẤTToolStripMenuItem_Click);
             // 
@@ -108,36 +124,38 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripStatusLabel2});
-            this.statusStrip1.Location = new System.Drawing.Point(147, 540);
+            this.statusStrip1.Location = new System.Drawing.Point(175, 723);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(966, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1097, 26);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(99, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(123, 20);
             this.toolStripStatusLabel1.Text = "Hello <Account>";
             this.toolStripStatusLabel1.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
             // 
             // FormMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1113, 562);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = global::QLHFC.Properties.Resources.download__10_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1272, 749);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormMenu";
@@ -161,5 +179,6 @@
         private StatusStrip statusStrip1;
         private ToolStripStatusLabel toolStripStatusLabel1;
         private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripMenuItem hÓAĐƠNToolStripMenuItem1;
     }
 }
