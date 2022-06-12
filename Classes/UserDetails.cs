1255811bbe99ecfe4d12a95b49201e14c7221b0b
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-12 20:23:11 +0700

    Create UserDetails.cs

---
+++ Classes/UserDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHFC.Classes
+{
+    public class UserDetails
+    {
+        private static string username;
+        private static string tennv;
+        public static string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public static string Tennv
+        {
+            get { return tennv; }
+            set { tennv = value; }
+        }
+    }
+}
