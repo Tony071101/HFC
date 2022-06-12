f07a7704aa6950eba8672d637e3c4e2dcb34d6d5
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-12 20:24:09 +0700

    Update FormLogin.cs

--- PresentationTier/FormLogin.cs
+++ PresentationTier/FormLogin.cs
@@ -1,6 +1,7 @@
 ﻿using QLHFC.PresentationTier;
 using MySql.Data.MySqlClient;
 using System.Data;
+using QLHFC.Classes;
 
 namespace QLHFC
 {
@@ -17,22 +18,30 @@ namespace QLHFC
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            //Đăng nhập 
-            FormMenu f;
+            //MySQL connection to retrieve user details into a class on successful log in
             conn.Open();
+            //Get count, username
             string dangnhap = "SELECT * FROM hfc.nhanvien WHERE Username= '" + txtUsername.Text + "' and Password = '" + txtPassword.Text + "'";
             MySqlCommand command = new MySqlCommand(dangnhap, conn);
-            adap = new MySqlDataAdapter(dangnhap, conn);
+            command.ExecuteNonQuery();
             DataTable dt = new DataTable();
-            adap.Fill(dt);
+            MySqlDataAdapter da = new MySqlDataAdapter(command);
+            da.Fill(dt);
             MySqlDataReader dr = command.ExecuteReader();
             if (dr.Read() == true)
             {
-                if(dt.Rows.Count > 0){
-                    f = new FormMenu(dt.Rows[0][0].ToString(), dt.Rows[0][1].ToString(), dt.Rows[0][2].ToString(), dt.Rows[0][3].ToString(), dt.Rows[0][4].ToString(), dt.Rows[0][5].ToString());
+                if (dt.Rows.Count > 0)
+                {
+                    //If the user exist - allow to log in
+                    //Store the information from the query to UserDetails class to be used in other forms
+                    UserDetails.UserName = dt.Rows[0][4].ToString();
+                    UserDetails.Tennv = dt.Rows[0][1].ToString();
+                    //Hide this form and open the main Dashboard Form
                     this.Hide();
+                    FormMenu f = new FormMenu(dt.Rows[0][0].ToString(), dt.Rows[0][1].ToString(), dt.Rows[0][2].ToString(), dt.Rows[0][3].ToString(), dt.Rows[0][4].ToString(), dt.Rows[0][5].ToString());
                     f.ShowDialog();
                 }
+                //If failed login - show message
                 else
                 {
                     MessageBox.Show("Vui lòng điền đầy đủ thông tin");
@@ -40,7 +49,7 @@ namespace QLHFC
             }
             else
             {
-                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không hợp lệ, vui lòng nhập lại.","Đăng nhập không thành công.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không hợp lệ, vui lòng nhập lại.", "Đăng nhập không thành công.", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtUsername.Text = "";
                 txtPassword.Text = "";
                 txtUsername.Focus();
@@ -48,6 +57,54 @@ namespace QLHFC
             }
             conn.Close();
             this.Show();
+            ////Check whether user exists
+            //if (dt.Rows.Count > 0)
+            //{
+            //    //If the user exist - allow to log in
+            //    //Store the information from the query to UserDetails class to be used in other forms
+            //    UserDetails.UserName = dt.Rows[0][4].ToString();
+            //    UserDetails.Tennv = dt.Rows[0][2].ToString();
+            //    //Hide this form and open the main Dashboard Form
+            //    this.Hide();
+            //    FormMenu f = new FormMenu(dt.Rows[0][0].ToString(), dt.Rows[0][1].ToString(), dt.Rows[0][2].ToString(), dt.Rows[0][3].ToString(), dt.Rows[0][4].ToString(), dt.Rows[0][5].ToString());
+            //    f.ShowDialog();
+            //}
+            ////If failed login - show message
+            //else
+            //{
+            //    MessageBox.Show("Login failed", "Technical - Login Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //}
+            //Đăng nhập 
+            //FormMenu f;
+            //conn.Open();
+            //string dangnhap = "SELECT * FROM hfc.nhanvien WHERE Username= '" + txtUsername.Text + "' and Password = '" + txtPassword.Text + "'";
+            //MySqlCommand command = new MySqlCommand(dangnhap, conn);
+            //adap = new MySqlDataAdapter(dangnhap, conn);
+            //DataTable dt = new DataTable();
+            //adap.Fill(dt);
+            //MySqlDataReader dr = command.ExecuteReader();
+            //if (dr.Read() == true)
+            //{
+            //    if(dt.Rows.Count > 0){
+            //        f = new FormMenu(dt.Rows[0][0].ToString(), dt.Rows[0][1].ToString(), dt.Rows[0][2].ToString(), dt.Rows[0][3].ToString(), dt.Rows[0][4].ToString(), dt.Rows[0][5].ToString());
+            //        this.Hide();
+            //        f.ShowDialog();
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Vui lòng điền đầy đủ thông tin");
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Tên đăng nhập hoặc mật khẩu không hợp lệ, vui lòng nhập lại.","Đăng nhập không thành công.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            //    txtUsername.Text = "";
+            //    txtPassword.Text = "";
+            //    txtUsername.Focus();
+            //    this.Show();
+            //}
+            //conn.Close();
+            //this.Show();
         }
         private void btnExit_Click(object sender, EventArgs e)
         {
