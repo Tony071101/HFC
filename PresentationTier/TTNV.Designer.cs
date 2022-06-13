08f173a86deb4d3566943ff677441ada9a224ac3
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-13 23:02:08 +0700

    Create TTNV.Designer.cs

---
+++ PresentationTier/TTNV.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace QLHFC.PresentationTier
+{
+    partial class TTNV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtName_NV = new System.Windows.Forms.TextBox();
+            this.txtAddress_NV = new System.Windows.Forms.TextBox();
+            this.txtSDT_NV = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Turquoise;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 206);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(299, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Họ Tên Nhân viên:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(299, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Địa Chỉ:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(299, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 28);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SĐT:";
+            // 
+            // txtName_NV
+            // 
+            this.txtName_NV.BackColor = System.Drawing.Color.Turquoise;
+            this.txtName_NV.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName_NV.Location = new System.Drawing.Point(492, 12);
+            this.txtName_NV.Name = "txtName_NV";
+            this.txtName_NV.ReadOnly = true;
+            this.txtName_NV.Size = new System.Drawing.Size(246, 34);
+            this.txtName_NV.TabIndex = 0;
+            // 
+            // txtAddress_NV
+            // 
+            this.txtAddress_NV.BackColor = System.Drawing.Color.Turquoise;
+            this.txtAddress_NV.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAddress_NV.Location = new System.Drawing.Point(392, 94);
+            this.txtAddress_NV.Name = "txtAddress_NV";
+            this.txtAddress_NV.ReadOnly = true;
+            this.txtAddress_NV.Size = new System.Drawing.Size(346, 34);
+            this.txtAddress_NV.TabIndex = 10;
+            // 
+            // txtSDT_NV
+            // 
+            this.txtSDT_NV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSDT_NV.BackColor = System.Drawing.Color.Turquoise;
+            this.txtSDT_NV.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSDT_NV.Location = new System.Drawing.Point(360, 185);
+            this.txtSDT_NV.Name = "txtSDT_NV";
+            this.txtSDT_NV.ReadOnly = true;
+            this.txtSDT_NV.Size = new System.Drawing.Size(378, 34);
+            this.txtSDT_NV.TabIndex = 11;
+            // 
+            // TTNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(750, 231);
+            this.Controls.Add(this.txtSDT_NV);
+            this.Controls.Add(this.txtAddress_NV);
+            this.Controls.Add(this.txtName_NV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "TTNV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TTNV";
+            this.Load += new System.EventHandler(this.TTNV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label6;
+        private TextBox txtName_NV;
+        private TextBox txtAddress_NV;
+        private TextBox txtSDT_NV;
+    }
+}
