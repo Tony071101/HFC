95756c334f61ffb5526cc14d42f12d65cb41318c
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-13 23:02:14 +0700

    Update Resources.Designer.cs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace QLHFC.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _252646_info_512x512 {
+            get {
+                object obj = ResourceManager.GetObject("252646_info_512x512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap download {
             get {
                 object obj = ResourceManager.GetObject("download", resourceCulture);
