ae9afd82a70ca03b3eeb0fe57f02d45ccdfdff73
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-21 13:49:25 +0700

    Create FormCheckinCheckout.cs

---
+++ PresentationTier/FormCheckinCheckout.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace QLHFC.PresentationTier
+{
+    public partial class FormCheckinCheckout : Form
+    {
+        MySqlConnection conn;
+        MySqlCommandBuilder cmd;
+        MySqlDataAdapter adap;
+        DataTable mytable;
+        string strconn = "Server = localhost; Port = 3306; Database = hfc; UId = root; Pwd = bjergsen07112001;";
+        string query_select = "SELECT * FROM hfc.time";
+        public FormCheckinCheckout()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCheckinCheckout_Load(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection(strconn);
+            conn.Open();
+            adap = new MySqlDataAdapter(query_select, conn);
+            cmd = new MySqlCommandBuilder(adap);
+            mytable = new DataTable();
+            adap.Fill(mytable);
+            conn.Close();
+            dgvCheckinCheckout.DataSource = mytable;
+        }
+    }
+}
