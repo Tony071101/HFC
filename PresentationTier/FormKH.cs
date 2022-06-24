1746c7b33f78bfb079baa228f8fb2449763c3bc1
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-24 14:18:20 +0700

    Update FormKH.cs

--- PresentationTier/FormKH.cs
+++ PresentationTier/FormKH.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using QLHFC.Classes;
+using System.IO;
+using OfficeOpenXml;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace QLHFC.PresentationTier
 {
@@ -27,7 +30,9 @@ namespace QLHFC.PresentationTier
         }
         private void FormKH_Load(object sender, EventArgs e)
         {
+            //
             //Show database
+            //
             try
             {
                 conn = new MySqlConnection(strconn);
@@ -55,7 +60,9 @@ namespace QLHFC.PresentationTier
             dgvKH.Columns[2].HeaderText = "Địa chỉ Khách hàng";
             dgvKH.Columns[3].HeaderText = "SĐT cá nhân";
         }
+        //
         //Hàm thêm
+        //
         private void btnAddKH_Click(object sender, EventArgs e)
         {
             conn.Open();
@@ -65,7 +72,9 @@ namespace QLHFC.PresentationTier
             conn.Close();
             Read_Data();
         }
+        //
         //Hàm xóa
+        //
         private void btnDelKH_Click(object sender, EventArgs e)
         {
             conn.Open();
@@ -75,7 +84,9 @@ namespace QLHFC.PresentationTier
             conn.Close();
             Read_Data();
         }
+        //
         //Hàm sửa
+        //
         private void btnEditKH_Click(object sender, EventArgs e)
         {
             conn.Open();
@@ -85,7 +96,9 @@ namespace QLHFC.PresentationTier
             conn.Close();
             Read_Data();
         }
+        //
         //Cập nhật dữ liệu tức thời
+        //
         private void Read_Data()
         {
             conn.Open();
@@ -96,7 +109,9 @@ namespace QLHFC.PresentationTier
             conn.Close();
             dgvKH.DataSource = mytable;
         }
+        //
         //Đọc dữ liệu từ datagridview ra textbox/combobox/picturebox
+        //
         private void dgvKH_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             DataGridViewRow row = new DataGridViewRow();
@@ -107,7 +122,9 @@ namespace QLHFC.PresentationTier
             txtSDT_KH.Text = Convert.ToString(row.Cells["SDT_KH"].Value);
             LSDH.Tenkh = txtNameKH.Text;
         }
+        //
         //Hàm tìm kiếm
+        //
         private void txtSearchKH_TextChanged(object sender, EventArgs e)
         {
             conn.Open();
@@ -125,6 +142,67 @@ namespace QLHFC.PresentationTier
         {
             FormLSDH f = new FormLSDH();
             f.Show();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //Excel.Application application = new Excel.Application();
+            //Excel.Workbook workbook;
+            //Excel.Worksheet worksheet;
+            //Excel.Range workrange;
+            //workbook = application.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            //worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Worksheets[1];
+            //workrange = (Microsoft.Office.Interop.Excel.Range)worksheet.Cells[1, 1];
+            //workrange.Range["A1:A1"].Value = "ID Khách hàng";
+            //workrange.Range["A1:B1"].Value = "Tên Khách hàng";
+            //workrange.Range["A1:C1"].Value = "Địa chỉ Khách hàng";
+            //workrange.Range["A1:D1"].Value = "SĐT cá nhân";
+            //for(int i =0; i <= dgvKH.Rows.Count - 1; i++)
+            //{
+            //    worksheet.Cells[1][i + 2] = i + 1; 
+            ////}
+            //SaveFileDialog save = new SaveFileDialog();
+            //save.Title = "Xuất file Excel";
+            //save.FileName = "";
+            //save.Filter = "Excel (*.xlsx)|*.xlsx|Excel 2003 (*.xls)|*.xls";
+            //if (save.ShowDialog() == DialogResult.OK)
+            //{
+            //    try
+            //    {
+            //        ExportExcel(save.FileName);
+            //        MessageBox.Show("Xuất file thành công.");
+            //    }
+            //    catch (Exception)
+            //    {
+            //        MessageBox.Show("Xuất file không thành công.");
+            //    }
+            //}
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ExportExcel (string path)
+        {
+            if (dgvKH.Rows.Count > 0)
+            {
+                Microsoft.Office.Interop.Excel.Application application = new Microsoft.Office.Interop.Excel.Application();
+                application.Application.Workbooks.Add(Type.Missing);
+                for (int i = 1; i < dgvKH.Columns.Count + 1; i++)
+                {
+                    application.Cells[1, i] = dgvKH.Columns[i - 1].HeaderText;
+                }
+                for (int i = 0; i < dgvKH.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dgvKH.Columns.Count; j++)
+                    {
+                        application.Cells[i + 2, j + 1] = dgvKH.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+                application.Columns.AutoFit();
+                application.Visible = true;
+            }
         }
     }
 }
