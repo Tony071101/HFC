e51e3cb47d739debf7b72bef52ea438dfe93a03c
Tonii <tinpigboyapple07112001@gmail.com>
2022-06-24 14:18:08 +0700

    Update FormHDNV.cs

--- PresentationTier/FormHDNV.cs
+++ PresentationTier/FormHDNV.cs
@@ -61,6 +61,7 @@ namespace QLHFC.PresentationTier
             dgvCTHD.Columns[6].HeaderText = "Giá món";
             dgvCTHD.Columns[7].HeaderText = "Khuyến mãi";
             dgvCTHD.Columns[8].HeaderText = "Tổng tiền";
+            pictureBox1.Visible = false;
         }
 
         private void dgvHD_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -82,68 +83,91 @@ namespace QLHFC.PresentationTier
         {
 
         }
-
+        //
+        //Hàm in
+        //
         private void btnPrint_Click(object sender, EventArgs e)
         {
-
             printPreviewDialog1.Document = printDocument1;
             printPreviewDialog1.ShowDialog();
         }
 
         private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            e.Graphics.DrawString("Hóa đơn", new Font("Arial", 40, FontStyle.Bold), Brushes.Red, new Point(300, 30));
-            e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Bold), Brushes.Black, new Point(0, 40));
-            e.Graphics.DrawString("Ngày in Hóa đơn: " + DateTime.Now.ToString("dd-MM-yyy hh:mm"), new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new Point(600, 70));
-            e.Graphics.DrawString("Số Hóa đơn: " + txtID_HD.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 120));
-            e.Graphics.DrawString("Họ tên Khách hàng: " + txtKH.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 150));
-            e.Graphics.DrawString("Nhân viên lập hóa đơn: " + txtNV.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 180));
-            e.Graphics.DrawString("Ngày lập Hóa đơn: " + txtDate.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 120));
-            e.Graphics.DrawString("Tên món ăn ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 210));
-            e.Graphics.DrawString("Giá món ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(750, 210));
-            e.Graphics.DrawString("Số lượng ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(270, 210));
-            e.Graphics.DrawString("Khuyến mãi ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 210));
-            e.Graphics.DrawString("" + txtTenMon.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 230));
-            e.Graphics.DrawString("" + txtGia.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(755, 230));
-            e.Graphics.DrawString("" + txtSL.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(300, 230));
-            e.Graphics.DrawString("" + txtKM.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(540, 230));
-            e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Regular), Brushes.Black, new Point(0, 200));
-            e.Graphics.DrawString("Thành tiền: " + txtTongtien.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(650, 270));
+            if(pictureBox1.Visible == false)
+            {
+                e.Graphics.DrawString("Hóa đơn", new Font("Arial", 40, FontStyle.Bold), Brushes.Red, new Point(300, 30));
+                e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Bold), Brushes.Black, new Point(0, 40));
+                e.Graphics.DrawString("Ngày in Hóa đơn: " + DateTime.Now.ToString("dd-MM-yyy hh:mm"), new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new Point(600, 70));
+                e.Graphics.DrawString("Số Hóa đơn: " + txtID_HD.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 120));
+                e.Graphics.DrawString("Họ tên Khách hàng: " + txtKH.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 150));
+                e.Graphics.DrawString("Nhân viên lập hóa đơn: " + txtNV.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 180));
+                e.Graphics.DrawString("Ngày lập Hóa đơn: " + txtDate.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 120));
+                e.Graphics.DrawString("Tên món ăn ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 210));
+                e.Graphics.DrawString("Giá món ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(750, 210));
+                e.Graphics.DrawString("Số lượng ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(270, 210));
+                e.Graphics.DrawString("Khuyến mãi ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 210));
+                e.Graphics.DrawString("" + txtTenMon.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 230));
+                e.Graphics.DrawString("" + txtGia.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(755, 230));
+                e.Graphics.DrawString("" + txtSL.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(300, 230));
+                e.Graphics.DrawString("" + txtKM.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(540, 230));
+                e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Regular), Brushes.Black, new Point(0, 200));
+                e.Graphics.DrawString("Thành tiền: " + txtTongtien.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(650, 270));
+            }
+            else
+            {
+                e.Graphics.DrawString("Hóa đơn", new Font("Arial", 40, FontStyle.Bold), Brushes.Red, new Point(300, 30));
+                e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Bold), Brushes.Black, new Point(0, 40));
+                e.Graphics.DrawString("Ngày in Hóa đơn: " + DateTime.Now.ToString("dd-MM-yyy hh:mm"), new Font("Arial", 10, FontStyle.Bold), Brushes.Black, new Point(600, 70));
+                e.Graphics.DrawString("Số Hóa đơn: " + txtID_HD.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 120));
+                e.Graphics.DrawString("Họ tên Khách hàng: " + txtKH.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 150));
+                e.Graphics.DrawString("Nhân viên lập hóa đơn: " + txtNV.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 180));
+                e.Graphics.DrawString("Ngày lập Hóa đơn: " + txtDate.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 120));
+                e.Graphics.DrawString("Tên món ăn ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 210));
+                e.Graphics.DrawString("Giá món ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(750, 210));
+                e.Graphics.DrawString("Số lượng ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(270, 210));
+                e.Graphics.DrawString("Khuyến mãi ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(500, 210));
+                e.Graphics.DrawString("" + txtTenMon.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(0, 230));
+                e.Graphics.DrawString("" + txtGia.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(755, 230));
+                e.Graphics.DrawString("" + txtSL.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(300, 230));
+                e.Graphics.DrawString("" + txtKM.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(540, 230));
+                e.Graphics.DrawString("_________________________________________________________", new Font("Arial", 40, FontStyle.Regular), Brushes.Black, new Point(0, 200));
+                e.Graphics.DrawString("Thành tiền: " + txtTongtien.Text, new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(650, 270));
+                e.Graphics.DrawString("Momo ", new Font("Arial", 13, FontStyle.Regular), Brushes.Black, new Point(700, 300));
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.DrawToBitmap(bmp, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+                e.Graphics.DrawImage(bmp, 630, 320);
+                bmp.Dispose();
+            }
             
         }
+        //
+        //Quét QR
+        //
         private void btnQR_Click(object sender, EventArgs e)
         {
-            Zen.Barcode.CodeQrBarcodeDraw codeQr = Zen.Barcode.BarcodeDrawFactory.CodeQr;
-            var qrText = txtID_HD.Text + "|" +
-            txtKH.Text + "|" +
-            txtNV.Text + "|" +
-            txtDate.Text + "|" +
-            txtTenMon.Text + "|" +
-            //txtGia.Text /*+ "|" +*/
-            txtSL.Text; /*+ "|" +*/
-            //txtKM.Text; /*+ "|" +*/
-            //Convert.ToString(txtTongtien.Text);
-            pictureBox1.Image = codeQr.Draw(qrText, 60);
-            //pictureBox1.Image = codeQr.Draw("{" + '"' + "Số Hóa đơn" + '"' + ":" + '"' + txtID_HD.Text + '"' + "," + '"' + "Họ tên Khách hàng" + '"' + ":" + '"' + txtKH.Text + '"' + "," + '"' + "Nhân viên lập hóa đơn" + '"' + ":" + '"' + txtNV.Text + '"' + '"' + "Ngày lập Hóa đơn" + '"' + ":" + '"' + txtDate.Text + "," + '"' + "Tên Món Ăn" + '"' + ":" + '"' + txtTenMon.Text + "," + '"' + "Giá món" + '"' + ":" + '"' + txtGia.Text + "," + '"' + "Số lượng" + '"' + ":" + '"' + txtSL.Text + "," + '"' + "Khuyến mãi" + '"' + ":" + '"' + txtKM.Text + "," + '"' + "Thành tiền" + '"' + ":" + '"' + txtTongtien.Text + '"' + "}", 900);
+            pictureBox1.Visible = true;
+            //Zen.Barcode.CodeQrBarcodeDraw codeQr = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            //var qrText = txtID_HD.Text + "|" +
+            //txtKH.Text + "|" +
+            //txtNV.Text + "|" +
+            //txtDate.Text + "|" +
+            //txtTenMon.Text + "|" +
+            ////txtGia.Text /*+ "|" +*/
+            //txtSL.Text; /*+ "|" +*/
+            ////txtKM.Text; /*+ "|" +*/
+            ////Convert.ToString(txtTongtien.Text);
+            //pictureBox1.Image = codeQr.Draw(qrText, 60);
+            ////pictureBox1.Image = codeQr.Draw("{" + '"' + "Số Hóa đơn" + '"' + ":" + '"' + txtID_HD.Text + '"' + "," + '"' + "Họ tên Khách hàng" + '"' + ":" + '"' + txtKH.Text + '"' + "," + '"' + "Nhân viên lập hóa đơn" + '"' + ":" + '"' + txtNV.Text + '"' + '"' + "Ngày lập Hóa đơn" + '"' + ":" + '"' + txtDate.Text + "," + '"' + "Tên Món Ăn" + '"' + ":" + '"' + txtTenMon.Text + "," + '"' + "Giá món" + '"' + ":" + '"' + txtGia.Text + "," + '"' + "Số lượng" + '"' + ":" + '"' + txtSL.Text + "," + '"' + "Khuyến mãi" + '"' + ":" + '"' + txtKM.Text + "," + '"' + "Thành tiền" + '"' + ":" + '"' + txtTongtien.Text + '"' + "}", 900);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            PrintDialog pd = new PrintDialog();
-            PrintDocument printDocument = new PrintDocument();
-            printDocument.PrintPage += PrintPic;
-            pd.Document = printDocument;
-            if(pd.ShowDialog() == DialogResult.OK)
-            {
-                printDocument.Print();
-            }
+
         }
         private void PrintPic(object sender, PrintPageEventArgs e)
         {
-            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.DrawToBitmap(bmp, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
-            e.Graphics.DrawImage(bmp, 0, 0);
-            bmp.Dispose();
+
         }
 
         private void dgvCTHD_CellContentClick(object sender, DataGridViewCellEventArgs e)
